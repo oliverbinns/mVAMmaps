dde932233f231c1e4c119f77961cec118c6b8c59
oliverbinns <oliverbinns@gmail.com>
2016-06-15 11:35:22 +0200

    Added selector dropdowns to mainpage

--- public_html/js/gui.js
+++ public_html/js/gui.js
@@ -1,7 +1,14 @@
-function selectAdmin0(val){
+function selectAdmin0(){
+	// Reset admin 1 selector
+	$('#ADM1select').val("null")
 
+	if($('#ADM0select').val() != 'null'){
+		// Pull country data
+
+
+	}
 }
 
-function selectAdmin1(val){
-	
+function selectAdmin1(){
+	console.log($('#ADM1select').val())
 }
