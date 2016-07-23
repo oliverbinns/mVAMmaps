edc68cafbbe038031aff122c7641ca00a8366cba
oliverbinns <oliverbinns@gmail.com>
2016-07-24 01:06:14 +0200

    Initial refactor of i/o code to deal with deparate country and region data pulls

--- public_html/js/io.js
+++ public_html/js/io.js
@@ -11,11 +11,14 @@ function APIpull(opt){
 
 	// Parse options and build query string
 	qs = "ADM0_NAME = '" + opt["adm0"] + "'"
-	if(typeof(opt["adm1"]) != "undefined" && opt["adm1"] != "Entire country"){
-		qs = qs + " AND AdminStrata = '" + opt["adm1"] + "'"
+	if(opt["adm1"] != "Entire country"){
+		// Select region data
+		escapedName = opt["adm1"].replace(/[']/g,"''")
+		qs = qs + " AND AdminStrata = '" + escapedName + "'"
 		qs = qs + " AND IndpVars = 'AdminUnits'"
 
-	} else if(typeof(opt["adm1"]) != "undefined" && opt["adm1"] == "Entire country") {
+	} else {
+		// Select country data
 		qs = qs + " AND AdminStrata = '" + opt["adm0"] + "'"
 		qs = qs + " AND IndpVars = 'ADM0'"
 	}
@@ -24,57 +27,101 @@ function APIpull(opt){
 	console.log("Requesting data from API:")
 	console.log("    " + qs + " | page = " + APIpage)
 
-	APIurl = "http://vam.wfp.org/mvam_monitoring/api.aspx"
-	APIdata = {
+	var APIurl = "http://vam.wfp.org/mvam_monitoring/api.aspx"
+	// Offline use
+		//var APIurl = "data/offlineAPIdata.json"
+	var APIdata = {
 		'table': "pblStatsSum", 
 		'where': qs,
 		'page': APIpage
 	}
 
-	// Offline use
-	//APIurl = "data/offlineAPIdata.json"
-
-	var request = $.ajax({
+	var reqOptions = {
 		type: "POST",
 		url: APIurl,
 		data: APIdata,
 		dataType: 'text'
-	});
-	
-	//Success - load the graphs with the data
-	request.done(function(msg) {
-		//Parse result and concatenate onto the response
-		SVRresp = JSON.parse(msg)
-		APIresponse = APIresponse.concat(SVRresp)
-		
-		console.log("API Data returned ok, " + SVRresp.length + " values, total " + APIresponse.length)
-		
-		// Check all data was returned in this page
-		if(SVRresp.length >= 999 ){
-			console.warn("Potentially not all data collected, getting next page")
-			APIpage++
-			APIpull(opt)
-		} else {
-			console.log("All data collected, processing...")
+	}
 
-			// Get min/max dates
-			APIdates = []
-			for (var i = 0; i < APIresponse.length; i++) {
-				//Convert to a moment timestamp
-				APIresponse[i].ts = moment.utc(APIresponse[i].SvyDate)
-				APIdates.push(APIresponse[i].ts)
+	if(opt["adm1"] == "Entire country"){
+		var requestCountry = $.ajax(reqOptions);
+
+		//Success - load the country-level data
+		requestCountry.done(function(msg) {
+			//Parse result and concatenate onto the response
+			SVRresp = JSON.parse(msg)
+			APIresponse = APIresponse.concat(SVRresp)
+			
+			console.log("API Data returned ok, " + SVRresp.length + " values, total " + APIresponse.length)
+			
+			// Check all data was returned in this page
+			if(SVRresp.length >= 999 ){
+				console.warn("Potentially not all data collected, getting next page")
+				APIpage++
+				APIpull(opt)
+			} else {
+				console.log("All data collected, processing...")
+
+				// Get min/max dates
+				var APIdates = []
+				for (var i = 0; i < APIresponse.length; i++) {
+					//Convert to a moment timestamp
+					APIresponse[i].ts = moment.utc(APIresponse[i].SvyDate)
+					APIdates.push(APIresponse[i].ts)
+				}
+				minDate = moment.min(APIdates)
+				maxDate = moment.max(APIdates)
+
+				updateGraphs()
 			}
-			minDate = moment.min(APIdates)
-			maxDate = moment.max(APIdates)
+		});
+		
+		//Fail - lof to onsole
+		requestCountry.fail(function(){
+			console.warn("Error getting country-level API data")
+			alert("Error getting data from the mVAM API.  Cannot continue.")
+			$('#loadDiv').fadeOut()
+		});
+	} else {
+		var requestRegion = $.ajax(reqOptions);
 
-			updateGraphs()
-		}
-	});
-	
-	//Fail - lof to onsole
-	request.fail(function(){
-		console.warn("Error getting API data")
-	});
+		//Success - load region-level data and update the graphs
+		requestRegion.done(function(msg) {
+			//Parse result and concatenate onto the response
+			SVRresp = JSON.parse(msg)
+			APIresponse = APIresponse.concat(SVRresp)
+			
+			console.log("API Data returned ok, " + SVRresp.length + " values, total " + APIresponse.length)
+			
+			// Check all data was returned in this page
+			if(SVRresp.length >= 999 ){
+				console.warn("Potentially not all data collected, getting next page")
+				APIpage++
+				APIpull(opt)
+			} else {
+				console.log("All data collected, processing...")
+
+				// Get min/max dates
+				var APIdates = []
+				for (var i = 0; i < APIresponse.length; i++) {
+					//Convert to a moment timestamp
+					APIresponse[i].ts = moment.utc(APIresponse[i].SvyDate)
+					APIdates.push(APIresponse[i].ts)
+				}
+				minDate = moment.min(APIdates)
+				maxDate = moment.max(APIdates)
+
+				updateGraphs()
+			}
+		});
+		
+		//Fail - lof to onsole
+		requestRegion.fail(function(){
+			console.warn("Error getting region-level API data")
+			alert("Error getting data from the mVAM API.  Cannot continue.")
+			$('#loadDiv').fadeOut()
+		});
+	}
 }
 
 function getRegions(){
