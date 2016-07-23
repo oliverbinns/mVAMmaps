591d8dcbd08a50be8b6531be4772db62f2da77e1
oliverbinns <oliverbinns@gmail.com>
2016-07-23 18:36:46 +0200

    Added GA logging for demo

--- public_html/js/init.js
+++ public_html/js/init.js
@@ -45,3 +45,11 @@ $(document).ready(function () {
 	getRegions()
 	initGraphs()
 });
+
+// Analytics logging (GA)
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-52581352-1');ga('send','pageview');
